ec42d7b1a6dc54d49f45e5f9fef9cfdc98722ac3
deepakpushpad <deepakpushpad@gmail.com>
2025-02-07 13:04:44 +0530

    adding list example

---
+++ Example_employee_List.py
@@ -0,0 +1,9 @@
+employees=[]
+employees = [["ID", "Name", "Salary" ], #Column Headrer
+                [1,"Deepak",5000],[2,"Aviraj",50005]]
+print(employees[2])
+print(employees[2][2])
+
+
+# draw back i to etract the data of an employee i have to remeber the index, and nned to check how this list is created like COlumn headers
+#Hard to read (data is stored in lists inside lists).
