daa4eb6a6849276d8179f9c4962b3baa1b9c1b11
deepakpushpad <deepakpushpad@gmail.com>
2025-03-01 23:25:31 +0530

    add reverse a string

---
+++ fib.py
@@ -0,0 +1,6 @@
+input_string = "This is an interview"
+splitinput = input_string.split()
+reverselinst=splitinput[::-1]
+output_string=" ".join(reverselinst)
+print(type(splitinput))
+print(output_string)
